a7847a770c37a3e446b6d21e937de8ed5b8677aa
PENTASHIFT <jwmunsey1@gmail.com>
2023-12-13 16:46:52 -0800

    Finals are done, time to catch up

---
+++ 3/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+#if defined(PART_ONE)
+
+unsigned int solve()
+{
+    std::string line;
+    unsigned int sum = 0;
+
+
+
+    return sum;
+}
+
+#elif defined(PART_TWO)
+
+
+#endif
+
+
+int main()
+{
+    std::cout << solve() << std::endl;
+    return 0;
+}
